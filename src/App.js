55729096f3718407a6b6a9ccd91f5ab52ec3339d
Michael Winser <mike.winser@gmail.com>
2021-03-10 12:40:12 -0800

    sort player cards by name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Context } from './Context';
 
 function App() {
   const {playerState, voteOff, removePlayer, changeLoyalty} = useContext(Context)
-  
+
   function handleVote(){
     const voteLog = voteOff()
     console.log("Vote-off called!")
@@ -16,11 +16,24 @@ function App() {
     console.log(voteLog.loserName + " voted off with " + voteLog.loserVotesAgainst + " votes.")
     removePlayer(voteLog.loserName)
   }
+
+  function comparePlayerNames(a,b){
+    const playerA = a.name.toUpperCase();
+    const playerB = b.name.toUpperCase();
+  
+    let comparison = 0;
+    if (playerA > playerB) {
+      comparison = 1;
+    } else if (playerA < playerB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
   
 
   return (
     <div className="fullscreen">
-      {playerState.sort().map((player, index)=>(
+      {playerState.sort(comparePlayerNames).map((player, index)=>(
           <PlayerCard key={index} player={player}/>
       ))
       }
