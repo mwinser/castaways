93247837cf8404c39a108c9a8e7921ba22a2823e
Michael Winser <mike.winser@gmail.com>
2021-03-12 16:07:02 -0800

    added stayed at camp and left camp events

--- src/randomEvent.js
+++ src/randomEvent.js
@@ -1,3 +1,102 @@
+const stayedAtCamp = [
+    {
+        string: " made everyone laugh.",
+        change: 5,
+        players: 1
+    },
+    {
+        string: " was appreciated for helping out around camp.",
+        change: 10,
+        players: 1
+    },
+    {
+        string: " burned the rice.",
+        change: -5,
+        players: 1
+    },
+    {
+        string: " was hanging out at camp too much",
+        change: -10,
+        players: 1
+    },
+    {
+        string: " let one of the chickens loose.",
+        change: -20,
+        players: 1
+    },
+    {
+        string: " found they had something in common.",
+        change: 5,
+        players: 2
+    },
+    {
+        string: " had a deep conversation.",
+        change: 10,
+        players: 2
+    },
+    {
+        string: " became friends.",
+        change: 20,
+        players: 2
+    },
+    {
+        string: " formed a secret alliance.",
+        change: 30,
+        players: 2
+    },
+    {
+        string: " had a minor disagreement.",
+        change: -5,
+        players: 2
+    },
+    {
+        string: " had a heated argument.",
+        change: -10,
+        players: 2
+    },
+    {
+        string: " had a major social fight.",
+        change: -20,
+        players: 2
+    },
+    {
+        string: " had a major falling-out.",
+        change: -30,
+        players: 2
+    }
+]
+const leftCamp = [
+    {
+        string: " refilled everyone's water bottles.",
+        change: 5,
+        players: 1
+    },
+    {
+        string: " was appreciated for bringing back firewood.",
+        change: 10,
+        players: 1
+    },
+    {
+        string: " forgot to refill water bottles.",
+        change: -5,
+        players: 1
+    },
+    {
+        string: " was gone for too long",
+        change: -10,
+        players: 1
+    },
+    {
+        string: " caught a wild chicken.",
+        change: 20,
+        players: 1
+    },
+    {
+        string: " found an immunity idol.",
+        idol: true
+    }
+]
+
 
 const onePlayerEvents = [
     {
@@ -70,4 +169,4 @@ const twoPlayerEvents = [
 ]
 
 
-export {onePlayerEvents, twoPlayerEvents}
+export {onePlayerEvents, twoPlayerEvents, leftCamp, stayedAtCamp}
